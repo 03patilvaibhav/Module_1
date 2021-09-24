f84fe7eb170fec5722051b28d52f3fbbfc74cb2a
03patilvaibhav <vpatil2442@gmail.com>
2021-09-24 21:41:07 +0530

    commit Module_1

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,6 @@
 package com.company;
 
- class helloworld {
+ class Main {
 
     public static void main(String[] args) {
 
---
+++ src/com/company/biker.java
@@ -0,0 +1,21 @@
+package com.company;
+
+class biker {
+    public static void main(String[] args) {
+        float b1=30, b2=35, b3=40,b4=35,b5=60,average;
+        average=b1+b2+b3+b4+b5/5;
+        System.out.println("Average=" +average);
+        if(b1>average)
+           System.out.println("b1 qualifies");
+       else if(b2>average)
+           System.out.println("b2 qualifies");
+       else if (b3>average)
+           System.out.println("b3 qualifies");
+       else if(b4>average)
+          System.out.print("b4 qualifies");
+       else if(b5>average)
+          System.out.println("b5 qualifies");
+        if(b1<average && b2<average && b3<average && b4<average && b5<average)
+          System.out.println("Nobody qualifies");
+}
+}
